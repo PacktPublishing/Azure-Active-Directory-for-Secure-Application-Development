6488fda28a13e4b0f1184a6264aab80ccef0a4f1
Sjoukje Zaal <sjoukje.zaal@sjoukjezaal.com>
2021-10-19 18:25:59 +0200

    Added ch6

---
+++ Chapter 6/PacktWebApp-OpenIDConnect-DotNet/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PacktWebApp_OpenIDConnect_DotNet.Models;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.Graph;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace PacktWebApp_OpenIDConnect_DotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly GraphServiceClient _graphServiceClient;
+       public HomeController(ILogger<HomeController> logger, GraphServiceClient graphServiceClient)
+        {
+        _logger = logger;
+        _graphServiceClient = graphServiceClient;
+        }
+
+         [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["ApiResult"] = user.DisplayName;
+
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> Profile()
+        {
+        var me = await _graphServiceClient.Me.Request().GetAsync();
+        ViewData["Me"] = me;
+
+        try
+        {
+            // Get user photo
+            using (var photoStream = await _graphServiceClient.Me.Photo.Content.Request().GetAsync())
+            {
+                byte[] photoByte = ((MemoryStream)photoStream).ToArray();
+                ViewData["Photo"] = Convert.ToBase64String(photoByte);
+            }
+        }
+        catch (System.Exception)
+        {
+            ViewData["Photo"] = null;
+        }
+
+        return View();
+        }
+      
+        public IActionResult Privacy()
+        {   
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
